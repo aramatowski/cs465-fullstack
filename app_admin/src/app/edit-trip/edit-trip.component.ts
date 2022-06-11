b63c29aafa780a9a7ec45ade3beeec4722c6d5a1
aramatowski <abby.ramatowski@snhu.edu>
2022-06-11 13:33:23 -0500

    Express website, static HTML only

---
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-trip',
+  templateUrl: './edit-trip.component.html',
+  styleUrls: ['./edit-trip.component.css']
+})
+export class EditTripComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // retrieve stashed tripId
+    let tripCode = localStorage.getItem("tripCode");
+    if (!tripCode) {
+      alert("Something wrong, couldn't find where I stashed tripCode!");
+      this.router.navigate(['']);
+      return;
+    }
+
+    // initialize form
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    })
+
+    this.tripService.getTrip(tripCode)
+      .then(data => {
+        console.log(data);
+        // Don't use editForm.setValue() as it will throw console error
+        this.editForm.patchValue(data);
+      })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.editForm.valid) {
+      this.tripService.updateTrip(this.editForm.value)
+        .then(data => {
+          console.log(data);
+          this.router.navigate(['']);
+        });
+    }
+  }
+
+}
